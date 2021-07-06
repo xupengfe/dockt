d4708a8e8f266c4f526bcad92c528175f8abd4c8
Pengfei Xu <pengfei.xu@intel.com>
2021-07-06 13:46:31 +0800

    test.sh: remove sleep and add guide print
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- hello/guide.sh
+++ hello/guide.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 TAG_NAME="h1"
-
+echo "Readme:"
 echo "How to install docker:https://docs.docker.com/engine/install/"
 echo "Or check https://github.com/xupengfe/dockt.git setup_syzkaller.sh for CentOS"
 
+echo "------"
 echo "docker build -t $TAG_NAME ."
 docker build -t $TAG_NAME .
 
+echo "------"
 echo "docker images"
 docker images
-
+echo "------"
+echo "Could remove comment '#' in Dockerfile ENTRYPOINT ["/test.sh"] for automation"
+echo "------"
 echo "docker run -it --privileged $TAG_NAME"
 docker run -it --privileged $TAG_NAME
--- hello/test.sh
+++ hello/test.sh
@@ -3,4 +3,3 @@
 echo "env | grep hello"
 env | grep hello
 echo "hello:$HELLO"
-sleep 60
