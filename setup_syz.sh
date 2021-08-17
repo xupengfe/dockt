67f622ef3957f4fd5037dcd3ed832e03cc4f5e60
Pengfei Xu <pengfei.xu@intel.com>
2021-08-17 10:37:11 +0800

    setup_syz.sh: could set up qemu next as default
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- setup_syz.sh
+++ setup_syz.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
 syzkaller_log="/tmp/syzkaller_setup"
+IMAGE:="/root/image/centos8.img"
+QEMU_NEXT="https://github.com/intel-innersource/virtualization.hypervisors.server.vmm.qemu-next"
+
+usage(){
+  cat <<__EOF
+  usage: ./${0##*/} [-s o|i] [-f [0|1] [-i 0|1] [-h]
+  -s  Source: o means official, i means intel-next
+  -f  Force: 0 will not reinstall if exist, 1 means will reinstall
+  -i  Ignore:0 will fully installation, 1 ignore rpm and image installation
+  -h  Help
+__EOF
+  exit 2
+}
 
 install_packages() {
+  [[ "$IGNORE" -eq 1 ]] && {
+    echo "IGNORE:$IGNORE is 1, will ignore rpm installation"
+    return 0
+  }
+
   cat /dev/null > $syzkaller_log
   echo "Install useful packages:"
   yum -y install glibc-devel.i686 glibc-devel
@@ -56,21 +74,52 @@ install_packages() {
   yum -y install  bridge-utils.x86_64
   yum -y install debootstrap.noarch
   yum -y install glibc.i686 --allowerasing
+  yum install -y ninja-build.x86_64
+  yum -y install screen
 }
 
 setup_qemu() {
   local qemu=""
+  local qemu_o="qemu"
+  local qemu_i="virtualization.hypervisors.server.vmm.qemu-next"
+
+  [[ "$FORCE" -eq 1 ]] {
+    echo "FORCE set to 1, will reinstall"
+    qemu=$(which qemu-system-x86_64)
+    echo " rm -rf $qemu"
+    rm -rf "$qemu"
+    qemu=""
+  }
 
   qemu=$(which qemu-system-x86_64)
   [[ -z "$qemu" ]] || {
-    echo "$qemu exist"
+    echo "$qemu exist and FORCE:$FORCE, no need reinstall"
     return 0
   }
 
   cd /root/
-  git clone https://github.com/qemu/qemu.git
-  cd qemu
-  git checkout -f v6.0.0
+
+  if [[ "$SOURCE" == 'o' ]]; then
+    rm -rf $qemu_o
+    git clone https://github.com/qemu/qemu.git
+    cd $qemu_o
+    git checkout -f v6.0.0
+  elif [[ "$SOURCE" == 'i' ]]; then
+    rm -rf $qemu_i
+    git clone $QEMU_NEXT
+    [[ $? -ne 0 ]] && {
+      echo "Could not get $QEMU_NEXT, please 'dt setup'!!!!"
+      echo "Could not get $QEMU_NEXT, please 'dt setup'!!!!" > $syzkaller_log
+      echo "Check $syzkaller_log"
+      exit 1
+    }
+    cd $qemu_i
+    git checkout -f origin/spr-beta
+  else
+    echo "Invalid SOURCE:$SOURCE, do nothing for qemu"
+    return 1
+  fi
+
   mkdir build
   cd build
   yum install -y ninja-build.x86_64
@@ -82,7 +131,12 @@ setup_qemu() {
 get_image() {
   img=""
 
-  img=$(ls /root/image/start2.sh 2>/dev/null)
+  [[ "$IGNORE" -eq 1 ]] && {
+    echo "IGNORE:$IGNORE is 1, will ignore image installation"
+    return 0
+  }
+
+  img=$(ls "$IMAGE" 2>/dev/null)
   [[ -z "$img" ]] || {
     echo "$img exist"
     return 0
@@ -90,6 +144,7 @@ get_image() {
 
   echo "Get the image"
   cd /root/
+  rm -rf image.tar.gz
   wget http://xpf-desktop.sh.intel.com/syzkaller/image.tar.gz
   tar -xvf image.tar.gz
 }
@@ -131,6 +186,7 @@ install_vncserver() {
 }
 
 next_to_do() {
+  echo "Set up log: $syzkaller_log"
   echo "Install syzkaller environment successfully. Next follow below to run syzkaller:"
   echo "$(date): The syzkaller environment has been set up successfully" > "$syzkaller_log"
   echo "cd /root/image"
@@ -138,12 +194,37 @@ next_to_do() {
 }
 
 main() {
-  install_packages
   setup_qemu
+  install_packages
   get_image
   install_syzkaller
   install_vncserver
   next_to_do
 }
+
+
+# Set detault value
+: "${SOURCE:=i}"
+: "${IGNORE:=0}"
+: "${FORCE:=0}
+while getopts :s:f:i:h arg; do
+  case $arg in
+    s)
+      SOURCE=$OPTARG
+      ;;
+    f)
+      FORCE=$OPTARG
+      ;;
+    i)
+      IGNORE=$OPTARG
+      ;;
+    h)
+      usage
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
 
 main
