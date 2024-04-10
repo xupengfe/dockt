58197b62707d0d81fbcc3288847386beea6c728d
Pengfei Xu <pengfei.xu@intel.com>
2024-04-10 14:22:52 +0800

    install_centos_docker: will install proxy automatically
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- install_centos_docker.sh
+++ install_centos_docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PROXY_FILE="/etc/systemd/system/docker.service.d/http-proxy.conf"
+
 echo "yum remove -y docker ..."
 yum remove -y docker \
                   docker-client \
@@ -25,3 +27,35 @@ yum install -y docker-ce docker-ce-cli containerd.io --allowerasing
 
 echo "systemctl restart docker"
 systemctl restart docker
+
+set_proxy() {
+  local check=""
+
+  mkdir -p /etc/systemd/system/docker.service.d
+  check=$(systemctl show --property=Environment docker | grep prc)
+  if [[ -z "$check" ]]; then
+    echo "No proxy for docker internal, will set docker intel proxy:"
+    echo "[Service]" > "$PROXY_FILE"
+    echo "Environment=\"HTTP_PROXY=http://proxy-prc.intel.com:913\"" >> "$PROXY_FILE"
+    echo "Environment=\"HTTPS_PROXY=http://proxy-prc.intel.com:913\"" >> "$PROXY_FILE"
+    echo "cat $PROXY_FILE"
+    cat "$PROXY_FILE"
+    echo "sudo systemctl daemon-reload"
+    sudo systemctl daemon-reload
+    echo "sudo systemctl restart docker"
+    sudo systemctl restart docker
+    check=""
+    check=$(systemctl show --property=Environment docker | grep prc)
+    if [[ -z "$check" ]]; then
+      echo "Failed to set proxy for docker: $check, please double check!"
+      echo "Check file:$PROXY_FILE"
+      echo "systemctl show --property=Environment docker | grep prc"
+    else
+      echo "After install, docker proxy info: $check"
+    fi
+  else
+    echo "Has docker proxy:$check"
+  fi
+}
+
+set_proxy
