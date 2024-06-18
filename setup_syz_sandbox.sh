4ee8fb4babb437c814eb77718d880680b486c1fa
Pengfei Xu <pengfei.xu@intel.com>
2024-06-18 09:26:52 +0800

    setup_syz_sandbox: no need print for check
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- setup_syz_sandbox.sh
+++ setup_syz_sandbox.sh
@@ -637,7 +637,7 @@ install_syzkaller() {
   check_run_syz=$(cat $bashrc | grep "check_run_syz")
   [[ -n "$check_run_syz" ]] || {
     echo "$(date) | Add /root/bzimage_bisect/check_run_syz.sh i in $bashrc" >> "$syzkaller_log"
-    echo "ps -ef | grep check_run_syz | grep sh || /root/bzimage_bisect/check_run_syz.sh i" >> $bashrc
+    echo "ps -ef | grep check_run_syz | grep sh 1>/dev/null || /root/bzimage_bisect/check_run_syz.sh i" >> $bashrc
   }
 
   check_syz=$(which syz-manager)
